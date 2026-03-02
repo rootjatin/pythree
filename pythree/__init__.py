ad93c4bf4ec386def3b53fa973114fbcf2433d81
Jatin Sharma <65252998+rootjatin@users.noreply.github.com>
2026-03-02 11:17:38 +0530

    Update __init__.py

---
+++ pythree/__init__.py
@@ -0,0 +1,112 @@
+# pythree/__init__.py
+"""
+pythree
+
+A lightweight 3D geometry / mesh package with helpers for:
+- safe math expression compilation
+- heightfield meshes
+- parametric surfaces
+- implicit surfaces
+- vector and mesh utilities
+"""
+
+from .sphere import Mesh
+from .equations import (
+    CompiledMathExpr,
+    compile_math_expr,
+    segments_from_step,
+    resolution_from_cell_size,
+    mesh_from_heightfield,
+    mesh_from_parametric,
+    mesh_from_implicit,
+    mesh_from_implicit_cell_size,
+)
+from .utils import (
+    vec_add,
+    vec_sub,
+    vec_mul,
+    vec_div,
+    dot,
+    cross,
+    length,
+    distance,
+    normalize,
+    lerp,
+    triangle_normal,
+    triangle_area,
+    triangle_centroid,
+    bounds_from_points,
+    bounds_union,
+    bounds_size,
+    bounds_center,
+    mesh_bounds,
+    mesh_centroid,
+    mesh_surface_area,
+    mesh_stats,
+    validate_faces,
+    validate_vertices,
+    validate_mesh,
+    translated,
+    scaled,
+    recentered,
+    merge_meshes,
+    remove_degenerate_faces,
+    unique_edges,
+    print_mesh_stats,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # core mesh type
+    "Mesh",
+
+    # equations
+    "CompiledMathExpr",
+    "compile_math_expr",
+    "segments_from_step",
+    "resolution_from_cell_size",
+    "mesh_from_heightfield",
+    "mesh_from_parametric",
+    "mesh_from_implicit",
+    "mesh_from_implicit_cell_size",
+
+    # vector utils
+    "vec_add",
+    "vec_sub",
+    "vec_mul",
+    "vec_div",
+    "dot",
+    "cross",
+    "length",
+    "distance",
+    "normalize",
+    "lerp",
+
+    # triangle utils
+    "triangle_normal",
+    "triangle_area",
+    "triangle_centroid",
+
+    # bounds utils
+    "bounds_from_points",
+    "bounds_union",
+    "bounds_size",
+    "bounds_center",
+
+    # mesh utils
+    "mesh_bounds",
+    "mesh_centroid",
+    "mesh_surface_area",
+    "mesh_stats",
+    "validate_faces",
+    "validate_vertices",
+    "validate_mesh",
+    "translated",
+    "scaled",
+    "recentered",
+    "merge_meshes",
+    "remove_degenerate_faces",
+    "unique_edges",
+    "print_mesh_stats",
+]
